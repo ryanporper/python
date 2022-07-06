44740e78169f7120a92a94bc29e9cf5721519ab1
ryanporper <ryan_porper@aol.com>
2022-07-06 18:57:36 -0400

    clean up

--- BankAcc/bankAccount.py
+++ BankAcc/bankAccount.py
@@ -16,7 +16,7 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Balance: {self.balance}")
+        print(f"Balance: ${self.balance}")
         return self
 
     def yield_interest(self):
