e1c173ce73c3cca6aea82f065382fa65a3773646
ryanporper <ryan_porper@aol.com>
2022-07-06 18:24:33 -0400

    updated user.py and complete bankAccount.py

--- userBankAcc/user.py
+++ userBankAcc/user.py
@@ -43,3 +43,36 @@ johnny = User("Johnny", "Jane", "jj452@gmail.com", "36")
 
 bill.display_info().enroll().spend_points(500)
 johnny.display_info().enroll().spend_points(50)
+
+
+# class BankAccount:
+#     def __init__(self, int_rate, balance):
+#         self.int_rate = int_rate
+#         self.balance = balance
+    
+#     def deposit(self, amount):
+#         self.balance += amount
+#         # print(f"Balance: {self.balance}")
+#         return self
+
+#     def withdraw(self, amount):
+#         self.balance -= amount
+#         # print(f"Balance: {self.balance}")
+#         return self
+
+#     def display_account_info(self):
+#         print(f"Balance: {self.balance}")
+#         return self
+
+#     def yield_interest(self):
+#         if self.balance > 0:
+#             self.balance += self.balance * self.int_rate
+#         else:
+#             pass
+#         return self
+
+# account1 = BankAccount(.05, 200)
+# account2 = BankAccount(.10, 400)
+
+# account1.deposit(100).deposit(150).deposit(200).withdraw(250).yield_interest().display_account_info()
+# account2.deposit(200).deposit(100).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yield_interest().display_account_info()
