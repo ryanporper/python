7aa6b0a2925fec6ee9f8085f1e1c714f3a5f1a1a
ryanporper <ryan_porper@aol.com>
2022-07-08 16:48:17 -0400

    HTML table assingment completed

--- hello_flask/server.py
+++ hello_flask/server.py
@@ -1,4 +1,4 @@
-from flask import Flask  
+from flask import Flask, render_template
 
 app = Flask(__name__)    
 
@@ -18,6 +18,16 @@ def say(word):
 @app.route('/repeat/<int:num>/<string:word>') 
 def repeat(num, word):
     return num * word
+
+@app.route('/lists')
+def render_lists():
+    users_info = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    return render_template("lists.html", users = users_info)
 
 if __name__=="__main__":   
     app.run(debug=True)    
