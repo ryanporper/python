c983c355f75d8734ae297f4d130fda29942ebad6
ryanporper <ryan_porper@aol.com>
2022-07-08 17:41:47 -0400

    uploading for loop basic

--- forLoops/for_loop_basic1.py
+++ forLoops/for_loop_basic1.py
@@ -1,43 +1,43 @@
-# for i in range(151):
-#     print(i)
+for i in range(151):
+    print(i)
 
 
-# print('start of multiples of five')
+print('start of multiples of five')
 
-# for i in range(5, 1000, 5):
-#     print(i)
+for i in range(5, 1000, 5):
+    print(i)
 
-# print('start of counting')
+print('start of counting')
 
 
-# for i in range(1, 100):
-#     if i % 10 == 0:
-#         print('Coding Dojo')
-#     elif i % 5 == 0:
-#         print('Coding')
-#     else:
-#         print(i)
+for i in range(1, 100):
+    if i % 10 == 0:
+        print('Coding Dojo')
+    elif i % 5 == 0:
+        print('Coding')
+    else:
+        print(i)
 
-# print('start of huge number')
+print('start of huge number')
 
-# sum = 0
-# for i in range(500000):
-#     if i % 2 != 0:
-#         sum += i
-# print(sum)
+sum = 0
+for i in range(500000):
+    if i % 2 != 0:
+        sum += i
+print(sum)
 
-# print('start of countdown by fours')
+print('start of countdown by fours')
 
-# for i in range(2018, 0, -4):
-#     print(i)
+for i in range(2018, 0, -4):
+    print(i)
 
-# print('start of flexible counter')
+print('start of flexible counter')
 
-# lowNum = 2
-# highNum = 9
-# mult = 3
+lowNum = 2
+highNum = 9
+mult = 3
 
-# for lowNum in range(lowNum, highNum, mult):
-#     print(lowNum)
-    # if lowNum % mult == 0:
-    #     print(lowNum)
+for lowNum in range(lowNum, highNum, mult):
+    print(lowNum)
+    if lowNum % mult == 0:
+        print(lowNum)
